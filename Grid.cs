4556749b6e3c5ea5d34e0ccfc24704b37283a6bb
eps0003 <eps0003@users.noreply.github.com>
2020-05-18 15:00:03 +1000

    Added Grid class

---
+++ Grid.cs
@@ -0,0 +1,60 @@
+﻿namespace sudoku_swe20001
+{
+    public class Grid
+    {
+        private readonly Cell[,] _cells;
+
+        public Grid()
+        {
+            _cells = new Cell[9, 9];
+            for (int x = 0; x < 9; x++)
+            {
+                for (int y = 0; y < 9; y++)
+                {
+                    _cells[x, y] = new Cell(x, y);
+                }
+            }
+        }
+
+        /// <summary>
+        /// Gets a cell
+        /// </summary>
+        /// <param name="x">x index of cell</param>
+        /// <param name="y">y index of cell</param>
+        /// <returns>A cell</returns>
+        public Cell GetCell(int x, int y)
+        {
+            if (IsValidCell(x, y))
+            {
+                return _cells[x, y];
+            }
+            return null;
+        }
+
+        /// <summary>
+        /// Sets a cell's value
+        /// </summary>
+        /// <param name="x">x index of cell</param>
+        /// <param name="y">y index of cell</param>
+        /// <param name="value">Value to set</param>
+        public void SetValue(int x, int y, int value)
+        {
+            Cell cell = GetCell(x, y);
+            if (cell != null)
+            {
+                cell.Value = value;
+            }
+        }
+
+        /// <summary>
+        /// Checks whether a grid position contains a cell
+        /// </summary>
+        /// <param name="x">x index of grid</param>
+        /// <param name="y">y index of grid</param>
+        /// <returns></returns>
+        private bool IsValidCell(int x, int y)
+        {
+            return x >= 0 && x < 9 && y >= 0 && y < 9;
+        }
+    }
+}
---
+++ TestGrid.cs
@@ -0,0 +1,42 @@
+﻿using NUnit.Framework;
+
+namespace sudoku_swe20001
+{
+    [TestFixture]
+    public class TestGrid
+    {
+        private Grid _grid;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _grid = new Grid();
+        }
+
+        [TestCase(0, 0)]
+        [TestCase(8, 8)]
+        public void TestGetValidCell(int x, int y)
+        {
+            Cell cell = _grid.GetCell(x, y);
+            Assert.IsNotNull(cell);
+        }
+
+        [TestCase(-1, 0)]
+        [TestCase(0, -1)]
+        [TestCase(9, 8)]
+        [TestCase(8, 9)]
+        public void TestGetInvalidCell(int x, int y)
+        {
+            Cell cell = _grid.GetCell(x, y);
+            Assert.IsNull(cell);
+        }
+
+        [TestCase(1, 1, 1)]
+        public void TestSetValue(int x, int y, int value)
+        {
+            _grid.SetValue(x, y, value);
+            int newValue = _grid.GetCell(x, y).Value;
+            Assert.AreEqual(value, newValue);
+        }
+    }
+}
