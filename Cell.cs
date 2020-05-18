ba0d18e11d4e2174fae0e783a1daa9ec9c9bd158
eps0003 <eps0003@users.noreply.github.com>
2020-05-18 14:21:37 +1000

    Added Cell class

---
+++ Cell.cs
@@ -0,0 +1,25 @@
+﻿namespace sudoku_swe20001
+{
+    public class Cell
+    {
+        public readonly int X;
+        public readonly int Y;
+        public int Value;
+
+        public Cell(int x, int y, int value = 0)
+        {
+            X = x;
+            Y = y;
+            Value = value;
+        }
+
+        /// <summary>
+        /// Checks whether the cell has a value
+        /// </summary>
+        /// <returns></returns>
+        public bool HasValue()
+        {
+            return Value > 0 && Value <= 9;
+        }
+    }
+}
---
+++ TestCell.cs
@@ -0,0 +1,33 @@
+﻿using NUnit.Framework;
+
+namespace sudoku_swe20001
+{
+    [TestFixture]
+    public class TestCell
+    {
+        private Cell _cell;
+
+        [SetUp]
+        public void SetUp()
+        {
+            _cell = new Cell(0, 0);
+        }
+
+        [TestCase(1)]
+        [TestCase(9)]
+        public void TestHasValidValue(int value)
+        {
+            _cell.Value = value;
+            Assert.IsTrue(_cell.HasValue());
+        }
+
+        [TestCase(-1)]
+        [TestCase(0)]
+        [TestCase(10)]
+        public void TestHasInvalidValue(int value)
+        {
+            _cell.Value = value;
+            Assert.IsFalse(_cell.HasValue());
+        }
+    }
+}
