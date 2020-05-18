3bdbfed206b47156a3e73918dba22f563fff74a3
eps0003 <eps0003@users.noreply.github.com>
2020-05-18 22:40:36 +1000

    Changed csv to txt

--- Program.cs
+++ Program.cs
@@ -8,7 +8,7 @@ namespace sudoku_swe20001
         {
             SplashKit.LoadFont("Roboto Regular", "fonts/Roboto-Regular.ttf");
             SplashKit.LoadFont("Roboto Bold", "fonts/Roboto-Bold.ttf");
-            Puzzles.LoadPuzzles("sudoku.csv");
+            Puzzles.LoadPuzzles("puzzles.txt");
 
             Window window = new Window("Sudoku", 800, 600);
             Sudoku sudoku = new Sudoku(Puzzles.GetRandomPuzzle(), SplashKit.ScreenCenter());
